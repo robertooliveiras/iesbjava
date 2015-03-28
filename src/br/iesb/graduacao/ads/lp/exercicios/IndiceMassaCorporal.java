350faca1a437c6312e3d1096bceda192342afcdc
RobertoOliveiras <robertooliveira2604@gmail.com>
2015-03-28 19:10:54 -0300

    Entrega do exercício 1 (denovo)

--- src/br/iesb/graduacao/ads/lp/exercicios/IndiceMassaCorporal.java
+++ src/br/iesb/graduacao/ads/lp/exercicios/IndiceMassaCorporal.java
@@ -18,25 +18,111 @@ import java.util.Scanner;
  * @author RobertoOliveira
  *
  */
+
+
 public class IndiceMassaCorporal {
+	
+	private TabelaIMC tIMC;
+	
+	enum TabelaIMC { MAP(1,"Muito Abaixo do Peso","[Frase preocupante]")
+				, AbP(2,"Abaixo do Peso","[Frase apreensiva]")
+				, PNo(3,"Peso Normal","[frase contente]")
+				, AcP(4,"Acima do Peso","[Frase apreensiva]")
+				, OI(5,"Obesidade I","[Frase muito apreensiva]")
+				, OII(6,"Obesidade II","[Frase preocupante]")
+				, OIII(7,"Obesidade III","[Frase ainda ais preocupante]" );
+		private int size;
+		private String description, frase;
+		TabelaIMC(int s, String d, String f){
+				this.size = s;
+				this.description = d;
+				this.frase = f;
+		}
+		
+		public int getSize(){
+			return size;
+		}
+
+		public String getDescription(){
+			return description;
+		}
+
+		public String getFrase(){
+			return frase;
+		}
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
+		float IMC, altura, peso;
+        String nome;
+		
 		Scanner entrada = new Scanner(System.in);
-        float IMC, altura, peso;
+        System.out.println("Bom dia!\n" +
+        				   "Sou o dr. Jarvis.\n\n" +
+        				   "Qual é o seu nome?"); 
         
-        System.out.println("Informe a altura (metros): ");
+        nome = entrada.next();
+        
+        System.out.println(nome +
+        				   ", estou aqui para avaliar seu Indice de Massa Corporal (IMC).\n" +
+        				   "Você precisará responder apenas duas perguntas!\n\n" +
+        				   "Não vale mentir, heim!\n\n");
+        
+    	
+        System.out.println("Qual é a sua altura em metros? ");
         altura = entrada.nextFloat();
         
-        System.out.println("Informe o peso (kg): ");
+        System.out.println("Qual é o seu peso em kg?: ");
         peso = entrada.nextFloat();
         
         entrada.close();
         
         IMC = peso / (altura * altura);
-        System.out.println("O IMC de uma pessoa que pesa "+peso+" kg e tem "+altura+"m de altura é : " + IMC);
+
+        IndiceMassaCorporal myIMC = new IndiceMassaCorporal();        
+        if(IMC <= 17){
+        	myIMC.tIMC = TabelaIMC.MAP;
+        }else if(IMC > 17 && IMC <= 18.49){
+        	myIMC.tIMC = TabelaIMC.AbP;
+        }else if(IMC > 18.49 && IMC <= 24.99){
+        	myIMC.tIMC = TabelaIMC.PNo;
+        }else if(IMC > 24.99 && IMC <= 29.99){
+        	myIMC.tIMC = TabelaIMC.AcP;
+        }else if(IMC > 29.99 && IMC <= 34.99){
+        	myIMC.tIMC = TabelaIMC.OI;
+        }else if(IMC > 34.99 && IMC <= 39.99){
+        	myIMC.tIMC = TabelaIMC.OII;
+        }else if(IMC > 39.99){
+        	myIMC.tIMC = TabelaIMC.OIII;
+        }
+        
+        System.out.println("Pesando "+peso+" kg com "+altura+"m de altura, O seu IMC é igual a " + IMC);
+        System.out.println("O seu estado físico é "+myIMC.tIMC.getDescription());        
+        System.out.println(myIMC.tIMC.getFrase());
+
+        System.out.println("\n\nVeja abaixo os níveis de estado físico registrados na br.iesb.java.Ex1");
+
+        
+        for(TabelaIMC mi: TabelaIMC.values()){
+    		System.out.println(mi.getSize() + " = " + mi.getDescription() );
+    	}
+	}
+
+	/**
+	 * @return the t_imc
+	 */
+	public TabelaIMC getTIMC() {
+		return tIMC;
+	}
+
+	/**
+	 * @param t_imc the t_imc to set
+	 */
+	public void setTIMC(TabelaIMC tIMC) {
+		this.tIMC = tIMC;
 	}
 }
